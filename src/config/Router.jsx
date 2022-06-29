0ef0fdf9bd9fd09cb50356efc58ebe07142689b4
Jessé Martins <jessemartins.ti@gmail.com>
2022-06-29 08:25:20 -0300

    feat:finalização da página sobre do projeto

--- src/config/Router.jsx
+++ src/config/Router.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 
 import Home from '../views/home/Home'
+import About from '../views/about/About'
 
 export default function Router() {
     return (
         <React.Fragment>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
             </Routes>
         </React.Fragment>
     )
---
+++ src/views/about/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './About.css'
+
+import ImgAbout from '../../assets/images/img-03.png'
+
+export default function About() {
+  return (
+    <React.Fragment>
+        <section className="rt-about">
+            <div className="container">
+                <div className="row d-flex align-items-center">
+                    <div className="col-lg-6">
+                        <img src={ImgAbout} />
+                    </div>
+                    <div className="col-lg-6">
+                        <h2>TRAZEMOS VIDA À SUA BICICLETA</h2>
+                        <p>Clique aqui e comece a digitar. Molestias excepturi sint occaecati cupiditate non provident similique sunt in culpa qui officia deserunt mollitia animi id est laborum et dolorum fuga et harum quidem.</p>
+                        <p>Voluptates repudiandae sint et molestiae non recusandae itaque earum rerum hic tenetur a sapiente delectus ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat sed ut perspiciatis unde.</p>
+                        <p>Eum fugiat quo voluptas nulla pariatur at vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </React.Fragment>
+  )
+}
