3b2c3364f08180d381459cea4607414dcadfad03
Jessé Martins <jessemartins.ti@gmail.com>
2022-06-29 08:50:05 -0300

    feat:finalização da página de serviços do projeto

--- src/config/Router.jsx
+++ src/config/Router.jsx
@@ -3,6 +3,7 @@ import { Routes,Route } from 'react-router-dom'
 
 import Home from '../views/home/Home'
 import About from '../views/about/About'
+import Service from '../views/service/Service'
 
 export default function Router() {
     return (
@@ -10,6 +11,7 @@ export default function Router() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/service" element={<Service />} />
             </Routes>
         </React.Fragment>
     )
---
+++ src/views/service/Service.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './Service.css'
+
+export default function Service() {
+  return (
+    <React.Fragment>
+        <section className="rt-service">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <h2>O QUE FAZEMOS PELA SUA BICICLETA</h2>
+                    </div>
+                    <div className="col-lg-6">
+                        <h4>Renovação completa</h4>
+                        <p>Clique aqui e comece a digitar. Ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.</p>
+                        <h4>Acabamentos</h4>
+                        <p>Clique aqui e comece a digitar. Ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.</p>
+                        <h4>Peças de reposição e acessórios</h4>
+                        <p>Clique aqui e comece a digitar. Ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </React.Fragment>
+  )
+}
