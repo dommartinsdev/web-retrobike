d783f11a7a2b0f9d3ba6b6f0df6c3a615bd6679d
Jessé Martins <jessemartins.ti@gmail.com>
2022-06-29 08:57:48 -0300

    feat:implementação das páginas e rotas do projeto

--- src/config/Router.jsx
+++ src/config/Router.jsx
@@ -4,6 +4,8 @@ import { Routes,Route } from 'react-router-dom'
 import Home from '../views/home/Home'
 import About from '../views/about/About'
 import Service from '../views/service/Service'
+import Store from '../views/store/Store'
+import Contact from '../views/contact/Contact'
 
 export default function Router() {
     return (
@@ -12,6 +14,8 @@ export default function Router() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/service" element={<Service />} />
+                <Route path="/store" element={<Store />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </React.Fragment>
     )
---
+++ src/views/store/Store.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Store.css'
+
+export default function Store() {
+  return (
+    <div>Store</div>
+  )
+}
